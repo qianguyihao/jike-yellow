33e804e367e66d704f32732a09d0035507db2d39
qianguyihao <qianguyihao@gmail.com>
2021-12-29 14:46:14 +0800

    jpeg to jpg

--- output.js
+++ output.js
@@ -5,11 +5,11 @@ let arr = [];
 // 前1000
 for (let i = 001; i < 1000; i++) {
   if (i < 10) {
-    arr.push(`### 00${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-00${i}.jpeg)\n\n`);
+    arr.push(`### 00${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-00${i}.jpg)\n\n`);
   } else if (i < 99) {
-    arr.push(`### 0${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-0${i}.jpeg)\n\n`);
+    arr.push(`### 0${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-0${i}.jpg)\n\n`);
   } else {
-    arr.push(`### ${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-${i}.jpeg)\n\n`);
+    arr.push(`### ${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-${i}.jpg)\n\n`);
   }
 }
 
@@ -26,7 +26,7 @@ const fs = require('fs')
 let arr = [];
 
 for (let i = 1001; i < 2000; i++) {
-  arr.push(`### ${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-${i}.jpeg)\n\n`);
+  arr.push(`### ${i}：待补充\n\n![](https://img.smyhvae.com/jike-yellow-${i}.jpg)\n\n`);
 }
 
 fs.writeFile('jike-yellow_2000.txt', arr, err => {
